8bd58df5165b2f6af34bb0f549e8b85fc5ba18bc
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-05-22 19:23:46 +0330

    update style

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-logo" alt="logo" >Counter</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          0
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <a href='#' className='add-count'>Add Count</a>
+        <a href='#' className='del-count'>Lower Count</a>
       </header>
     </div>
   );
